4e49c31bcaa4ac1edd0ca60d1f6c0c69549169c3
nuesslerm <markus@choco.com>
2020-05-12 01:14:53 +0200

    done with feature

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 
+// hi
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
